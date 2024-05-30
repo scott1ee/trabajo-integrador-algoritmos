d3e61540388ed8a9aed2ef39addee73f893b3771
Herrera Franco <69646385+scott1ee@users.noreply.github.com>
2024-05-30 16:52:49 -0300

    primeras carpetas

---
+++ trabajo-practico-integrador-algoritmos/empresa/empresa.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Proyecto N°1
+ * Integrantes del Grupo: Ledesma Mauricio, Herrera Franco
+ *
+ * 
+ * 
+ */
+using System;
+
+namespace empresa
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ trabajo-practico-integrador-algoritmos/grupo-de-obreros/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fraan
+ * Date: 5/30/2024
+ * Time: 4:34 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace grupo_de_obreros
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
